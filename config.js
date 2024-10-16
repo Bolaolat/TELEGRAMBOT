a9124f545d878edb017e9972b60eea4c6e59ec85
Bolaolat <bolaolat300@gmail.com>
2024-10-16 17:45:15 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    telegramToken: '7210707261:AAH5KDmA65ld0zyfgXjuTl_6A7dnu_h0z6Q',
+    exchangeRateApiKey: '43f31fb84c391ced11b216a4',
+    openaiApiKey: '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' // Add this line
+};
